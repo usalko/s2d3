b9e6cb80d19c9747a0bd1c4bc68b2b72fb3b704c
Усалко Ваня <ivict@rambler.ru>
2024-03-23 23:35:44 +0300

    # There is the utils module (initial commit).

---
+++ utils/headers.go
@@ -0,0 +1,62 @@
+/**
+ * Author: Vanya Usalko <ivict@rambler.ru>
+ * File: headers.go
+ */
+package utils
+
+import (
+	"bytes"
+	"net/http"
+	"sort"
+	"strings"
+)
+
+func v2Headers(request *http.Request) []byte {
+	//TODO: using standard library
+	subset := make(map[string]string)
+	names := make([]string, 0)
+
+	for header := range request.Header {
+		LowerCaseHeader := strings.ToLower(header)
+		if strings.HasPrefix(LowerCaseHeader, "x-amz-") {
+			names = append(names, LowerCaseHeader)
+			subset[LowerCaseHeader] = strings.Trim(request.Header.Get(header), " \t\r\n\f") + "\n"
+		}
+	}
+	sort.Strings(names)
+
+	headersLengths := make([][]byte, len(names))
+	for i, header := range names {
+		headersLengths[i] = bytes.Join([][]byte{[]byte(header), []byte(subset[header])}, []byte{0x3a})
+	}
+	return bytes.Join(headersLengths, []byte{})
+}
+
+func v4Headers(request *http.Request) ([]byte, []byte) {
+	//TODO: using standard library
+	subset := make(map[string]string)
+	names := make([]string, 0)
+
+	for header := range request.Header {
+		lowerCaseHeader := strings.ToLower(header)
+		if lowerCaseHeader == "host" || strings.HasPrefix(lowerCaseHeader, "x-amz-") {
+			names = append(names, lowerCaseHeader)
+			subset[lowerCaseHeader] = strings.Trim(request.Header.Get(header), " \t\r\n\f")
+		}
+	}
+	sort.Strings(names)
+
+	headersLengths := make([][]byte, len(names))
+	headersValues := make([][]byte, len(names))
+	for i, header := range names {
+		headersValues[i] = []byte(header)
+		headersLengths[i] = bytes.Join([][]byte{headersValues[i], []byte(subset[header])}, []byte{0x3a})
+	}
+
+	signed := bytes.Join(headersValues, []byte{0x3b})
+	return signed, bytes.Join([][]byte{
+		bytes.Join(headersLengths, []byte{0x0a}),
+		nil, /* force an empty line */
+		signed,
+	}, []byte{0x0a})
+}
---
+++ utils/queries.go
@@ -0,0 +1,39 @@
+/**
+ * Author: Vanya Usalko <ivict@rambler.ru>
+ * File: queries.go
+ */
+package utils
+
+import (
+	"bytes"
+	"net/url"
+	"sort"
+	"strings"
+)
+
+func v4QueryString(queryString string) []byte {
+	//TODO: using standard library
+	if queryString == "" {
+		return []byte{}
+	}
+
+	queryParams := strings.Split(queryString, "&")
+	sort.Strings(queryParams)
+
+	queryParamsLengths := make([][]byte, len(queryParams))
+	for i := range queryParams {
+		keyValue := strings.SplitN(queryParams[i], "=", 2)
+		key := url.QueryEscape(keyValue[0])
+		var value []byte
+		if len(keyValue) == 2 {
+			value = []byte(url.QueryEscape(keyValue[1]))
+		}
+
+		queryParamsLengths[i] = make([]byte, len(key)+1+len(value))
+		copy(queryParamsLengths[i], key)
+		queryParamsLengths[i][len(key)] = 0x3d
+		copy(queryParamsLengths[i][1+len(key):], value)
+	}
+
+	return bytes.Join(queryParamsLengths, []byte{0x26})
+}
