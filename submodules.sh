0eeb93ea3a7850216fc31f6856aa2dfd89beb9bd
Усалко Ваня <ivict@rambler.ru>
2024-03-24 08:36:41 +0300

    # I've packed stuff knowledges.

--- submodules.sh
+++ submodules.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# @see https://stackoverflow.com/questions/67678203/why-does-go-get-fail-with-invalid-version-unknown-revision
+
+cd utils || exit
+GOPRIVATE=github.com/usalko/s2d3/utils go get github.com/usalko/s2d3/utils@v0.1.0-alpha.1
+cd ..
+
 GOPROXY=proxy.golang.org go list -m github.com/usalko/s2d3/client
 GOPROXY=proxy.golang.org go list -m github.com/usalko/s2d3/models
 GOPROXY=proxy.golang.org go list -m github.com/usalko/s2d3/services
