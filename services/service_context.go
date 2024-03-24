05ae515b6b52410ab2d582092923c79769fad0d3
Усалко Ваня <ivict@rambler.ru>
2024-03-24 18:15:41 +0300

    # There is the initial commit for an services module (v0.1.0-alpha.6).

--- services/service_context.go
+++ services/service_context.go
@@ -5,5 +5,36 @@
 
 package services
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/usalko/s2d3/models"
+)
+
 type ServiceContext struct {
+}
+
+type ServiceContextKey string
+
+const KeyServerAddr ServiceContextKey = "serverAddr"
+
+func GetRoot(writer http.ResponseWriter, request *http.Request) {
+	ctx := request.Context()
+
+	bucket := models.Bucket{
+		Name: "",
+	}
+	println(&bucket)
+
+	fmt.Printf("%s: got / request\n", ctx.Value(KeyServerAddr))
+	io.WriteString(writer, "This is my website!\n")
+}
+
+func GetHello(writer http.ResponseWriter, request *http.Request) {
+	ctx := request.Context()
+
+	fmt.Printf("%s: got /hello request\n", ctx.Value(KeyServerAddr))
+	io.WriteString(writer, "Hello, HTTP!\n")
 }
